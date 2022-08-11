40e45a99b8d10baa3ed43068adc4a7f06903b484
ifuad <i.fuad.tm@gmail.com>
2022-08-11 21:42:46 +0400

    ref

--- scripts/windows-user-create.bat
+++ scripts/windows-user-create.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 curl -O https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
-net user Administrator %VNC_USER_PASSWORD% /add >nul
+net user Administrator $env:VNC_USER_PASSWORD /add >nul
 net localgroup administrators Administrator /add >nul
 net user Administrator /active:yes >nul
 diskperf -Y >nul
