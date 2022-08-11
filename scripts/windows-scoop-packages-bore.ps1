62a354bec54ab28e8da0dbd077ba204fbcbad7c8
ifuad <i.fuad.tm@gmail.com>
2022-08-11 19:32:59 +0400

    ref

--- scripts/windows-scoop-packages-bore.ps1
+++ scripts/windows-scoop-packages-bore.ps1
@@ -9,6 +9,6 @@ scoop install aria2 neovim
 scoop bucket add Syndim https://github.com/Syndim/scoop-bucket && scoop install Syndim/bore # ngrok alternative for tcp rdp # usage for rdp: bore local -p 3389 --to bore.pub 3389
 
 echo @'
-start /b bore local -p 3389 --to bore.pub 3389 >/dev/null &
+start /b %userprofile%\scoop\shims\bore local -p 3389 --to bore.pub 3389 >/dev/null &
 '@ > .\bore-hidden.bat
 .\bore-hidden.bat
