7e295e7a3a2d5bcf68ee2f69601e4a8f6640fbee
ifuad <i.fuad.tm@gmail.com>
2022-08-12 16:27:48 +0400

    ref pass

--- scripts/windows-user-create.bat
+++ scripts/windows-user-create.bat
@@ -3,7 +3,8 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 curl -O https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& net user Administrator $env:VNC_USER_PASSWORD /add >nul"
+@REM # PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& net user Administrator $env:VNC_USER_PASSWORD /add >nul"
+net user "Administrator" "%VNC_USER_PASSWORD%" /add
 net localgroup administrators Administrator /add >nul
 net user Administrator /active:yes >nul
 diskperf -Y >nul
